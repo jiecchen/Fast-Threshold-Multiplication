f8b472a3033b5e9f6917a037cb9720ba1c0835bd
CJC <jiecchen@indiana.edu>
2015-05-22 00:03:19 -0400

    change the order

--- createData.cpp
+++ createData.cpp
@@ -17,8 +17,8 @@ int main() {
   fill(P[0], P[0] + Pm * Pn, 0);
   for (int i = 0; i < nnz; ++i)
     P[rand() % Pm][rand() % Pn] = 1;
-  for (int i = 0; i < Pm; ++i)
-    for (int j = 0; j < Pn; ++j)
+  for (int j = 0; j < Pn; ++j)
+    for (int i = 0; i < Pm; ++i)
       if (P[i][j] > 0)
 	nonz.push_back(Element(i, j, 1));
   cout << Pm << " " << Pn << endl;
