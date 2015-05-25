7623b9b9a3f0c2578121d22f9f486d5071f500b9
CJC <jiecchen@indiana.edu>
2015-05-25 15:14:31 -0400

    format test

--- test.cpp
+++ test.cpp
@@ -7,50 +7,18 @@
 
 
 
+void test_mergeNeighbor(CMatrix_CSC &P) {
+  std::cout << "P = \n" << toCoo(P) << std::endl;
+  CMatrix_CSC Q = mergeNeighbor(P);
+  std::cout << "mergeNeighbor(P) = \n" << toCoo(Q) << std::endl;
+}
 
 
-
-void test_csc() {
-  // read matrix P
-  int Pm, Pn, nnz;
-  std::cin >> Pm >> Pn;
-  std::cin >> nnz;
-  int row[nnz];
-  int col[nnz];
-  int val[nnz];
-  std::vector<Element> arr;
-  for (int i = 0; i < nnz; ++i) {
-    std::cin >> row[i] >> col[i] >> val[i];
-    arr.push_back(Element(row[i], col[i], val[i]));
-  }   
-  CMatrix_COO coo(arr.begin(), arr.end(), Pm, Pn);
-  CMatrix_CSC csc(val, row, col, nnz, Pm, Pn);
-  
-  // int v[] = {0, 1, 2, 3, 4, 5};
-  
-  // SparseVector result;
-  // thresh_mult(result, csc, v, v + 6, 0);
-  std::cout << "mat = \n" << coo << std::endl;
-  //  std::cout << result << std::endl;
-    
-
-
-  // read matrix Q
-  int Qm, Qn;
-  std::cin >> Qm >> Qn;
-  std::cin >> nnz;
-  arr.clear();
-  for (int i = 0; i < nnz; ++i) {
-    std::cin >> row[i] >> col[i] >> val[i];
-    arr.push_back(Element(row[i], col[i], val[i]));
-  }   
-  CMatrix_COO Q(arr.begin(), arr.end(), Qm, Qn);
-  CMatrix_CSC new_csc(val, row, col, nnz, Qm, Qn);
-  std::cout << "new mat = \n" << Q << std::endl;
-  CMatrix_CSC qq = csc * new_csc;
-  Q = toCoo(qq);
-  std::cout << "mul = \n" << Q << std::endl;
-
+void test_csc(CMatrix_CSC &P, CMatrix_CSC &Q) {
+  std::cout << "P = \n" << toCoo(P) << std::endl;
+  std::cout << "Q = \n" << toCoo(Q) << std::endl;
+  CMatrix_CSC mult = P * Q;
+  std::cout << "P * Q = \n" << toCoo(mult) << std::endl;
 }
 
 
@@ -60,71 +28,30 @@ void test_csc() {
 
 
 
-
 int main() {
-  test_csc();
-  // // read matrix P
-  // int Pm, Pn, Qm, Qn, nnz;
-  // std::cin >> Pm >> Pn;
-  // std::cin >> nnz;
-  // int row, col, val;
-  // std::vector<Element> arr;
-  // for (int i = 0; i < nnz; ++i) {
-  //   std::cin >> row >> col >> val;
-  //   arr.push_back(Element(row, col, val));
-  // }   
-  // CMatrix_COO P(arr.begin(), arr.end(), Pm, Pn);
+  const int MAX_NNZ = 1000;
+  int Pm, Pn, nnz;
+  std::cin >> Pm >> Pn;
+  std::cin >> nnz;
+  int row[MAX_NNZ];
+  int col[MAX_NNZ];
+  int val[MAX_NNZ];
+  for (int i = 0; i < nnz; ++i) {
+    std::cin >> row[i] >> col[i] >> val[i];
+  }   
+  CMatrix_CSC P(val, row, col, nnz, Pm, Pn);
   
-  // // read matrix Q
-  // std::cin >> Qm >> Qn;
-  // std::cin >> nnz;
-  // arr.clear();
-  // for (int i = 0; i < nnz; ++i) {
-  //   std::cin >> row >> col >> val;
-  //   arr.push_back(Element(row, col, val));
-  // }   
-  // CMatrix_COO Q(arr.begin(), arr.end(), Qm, Qn);
-  
- 
-
-  // std::cout << "P = \n" << P << std::endl;
-  // std::cout << "Q = \n" << Q << std::endl;
-
-  // double theta = 2;
-  // double rho = 0.1;
-  // CMatrix_COO result = atLeastMult(P, Q, theta, rho);
-  // std::cout << "PQ = \n" << result << std::endl;
-  // result.print();
-
-  
-
-  // // try to construct a dyadic structure
-  // // and do recovering
-  // // CDyadicSketch dyadic;
-  // // dyadicSketch(dyadic, 0.1, 10, B);
-  // // int thresh = 201;
-  // // SparseVector result;
-  // // thresholdRecover(result, dyadic, vec.begin(), vec.end(), thresh);
-
-  
-  // // for (unsigned int i = 0; i < result.size(); ++i)
-  // //   std::cout << result[i].ind << ", " << result[i].val << std::endl;
-  // // std::cout << std::endl;
+  int Qm, Qn;
+  std::cin >> Qm >> Qn;
+  std::cin >> nnz;
+  for (int i = 0; i < nnz; ++i) {
+    std::cin >> row[i] >> col[i] >> val[i];
+  }   
+  CMatrix_CSC Q(val, row, col, nnz, Qm, Qn);
 
 
-
-  // // std::cout << B << std::endl;
-  // // std::cout << vec << std::endl;
-  // // SparseVector mulRes;
-  // // B.sortByRowColumn();
-  // // thresholdMult(mulRes, B, vec, 1);
-  // // std::cout << mulRes << std::endl;
-
-  
-  // // int *cm = sketchVector(sk, vec.begin(), vec.end());
-  // // for (int i  = 0; i < M; i++) 
-  // //   std::cout << recover(cm, sk, i) << std::endl;
-
+  test_csc(P, Q);
+  test_mergeNeighbor(P);
   return 0;
 }
 
