a2d8420a694f68654a1bd6f7a4370052832e6cf7
CJC <jiecchen@indiana.edu>
2015-05-26 15:27:08 -0400

    add timer

--- utils.h
+++ utils.h
@@ -1,8 +1,28 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+
+class CTimer {
+  clock_t _start;
+ public:
+  void start() { _start = std::clock(); }
+  void stop(std::string msg = "") {
+    std::cerr << msg +  " Elapsed: "
+	      << (std::clock() - _start) / (double) CLOCKS_PER_SEC
+	      << " secs" << std::endl;	      
+  }
+};
 
 
 #endif
+
+
+
+
+
+
 
 
 
