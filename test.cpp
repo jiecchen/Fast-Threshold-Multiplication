4b0644b3284ef1502fffd43f9dbe0e90fc119d94
CJC <jiecchen@indiana.edu>
2015-05-27 14:49:45 -0400

    updated

--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@
 #include <cmath>
 #include <numeric>
 #include <iomanip>
-
-
+#include <fstream>
+#include "utils.h"
 
 void test_mergeNeighbor(CMatrix_CSC &P) {
   std::cout << "P = \n" << toCoo(P) << std::endl;
@@ -23,19 +23,20 @@ void test_csc(CMatrix_CSC &P, CMatrix_CSC &Q) {
 
 
 
-
+const int MAX_NNZ = 2000000;
+int row[MAX_NNZ];
+int col[MAX_NNZ];
+int val[MAX_NNZ];
 
 
 
 
 int main() {
-  const int MAX_NNZ = 1000;
   int Pm, Pn, nnz;
+  CTimer timer;
+  timer.start();
   std::cin >> Pm >> Pn;
   std::cin >> nnz;
-  int row[MAX_NNZ];
-  int col[MAX_NNZ];
-  int val[MAX_NNZ];
   for (int i = 0; i < nnz; ++i) {
     std::cin >> row[i] >> col[i] >> val[i];
   }   
@@ -56,14 +57,20 @@ int main() {
     std::cin >> row[i] >> col[i] >> val[i];
   }   
   CMatrix_CSC W(val, row, col, nnz, Wm, Wn);
-
+  timer.stop("Read P Q W ");
   //  test_csc(P, Q);
   //  test_mergeNeighbor(P);
 
+  // std::cout << "P * Q = \n" << toCoo(P * Q) << std::endl 
+  // 	    << "Q * W = \n" << toCoo(Q * W) << std::endl;
+  timer.start();
+  // CMatrix_CSC ans = P * Q * W;
+  timer.stop("Calculate P * Q * W ");
+  //std::cout << "P * Q * W =\n" << toCoo(ans) << std::endl;
+  timer.start();
+  CMatrix_CSC A = FastThreshMult(P, Q, W, 3, 200000, 0.5);
   
-  std::cout << "P * Q * W =\n" << toCoo(P * Q * W) << std::endl;
-  //  std::cout << "r = " << calcL1Norm(P, Q, Q) << std::endl;
-  CMatrix_CSC A = FastThreshMult(P, Q, W, 59, 0.1);
+  timer.stop("FastThreshMult ");
   std::cout << toCoo(A) << std::endl;
   return 0;
 
