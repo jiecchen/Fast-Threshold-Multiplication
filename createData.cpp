9a9f50c6891d68a31eabfe5fa6fb8e54a99710de
CJC <jiecchen@indiana.edu>
2015-05-27 14:49:27 -0400

    add new logic

--- createData.cpp
+++ createData.cpp
@@ -6,24 +6,36 @@
 #include <random>
 #include <ctime>
 using namespace std;
+const int MAXN = 2000;
+int P[MAXN][MAXN];
+int Q[MAXN][MAXN];
+int W[MAXN][MAXN];
+
 
 int main() {
-  
-  default_random_engine g(time(NULL));
-  discrete_distribution<int> dist {39,18,11,11,2,2,1,1};
 
+  
+  mt19937 g(time(NULL));
+  discrete_distribution<int> dist {22,12,1,1,2,2,1,1,2,2};
 
   int Pm, Pn, Qn, Wn;
   int nnz;
   cerr << "P_m P_n Q_n Wn = ";
   cin >> Pm >> Pn >> Qn >> Wn;
+  int hPn, hQn;
+  hPn = Pn / 2;
+  hQn = Qn / 2;
+
   cerr << "nnz(P) = ";
   cin >> nnz;
-  int P[Pm][Pn];
+  //  int P[Pm][Pn];
   vector<Element> nonz;
-  fill(P[0], P[0] + Pm * Pn, 0);
-  for (int i = 0; i < nnz; ++i)
-    P[dist(g) % Pm][dist(g) % Pn] = 1;
+  //  fill(P[0], P[0] + Pm * Pn, 0);
+  for (int i = 0; i < nnz; ++i) {
+    P[0][rand() % hPn] = 1;
+    P[1 + (rand() % (Pm - 1))][hPn + rand() % hPn] = 1;
+  }
+
   for (int j = 0; j < Pn; ++j)
     for (int i = 0; i < Pm; ++i)
       if (P[i][j] > 0)
@@ -36,10 +48,14 @@ int main() {
   nonz.clear();
   cerr << "nnz(Q) = ";
   cin >> nnz;
-  int Q[Pn][Qn];
-  fill(Q[0], Q[0] + Pn * Qn, 0);
-  for (int i = 0; i < nnz; ++i)
-    Q[dist(g) % Pn][dist(g) % Qn] = 1;
+  //  int Q[Pn][Qn];
+  //  fill(Q[0], Q[0] + Pn * Qn, 0);
+  for (int i = 0; i < nnz; ++i) {
+    Q[rand() % hPn][rand() % hQn] = 1;   
+    Q[hPn + rand() % hPn][hQn + rand() % hQn] = 1;
+  }
+
+
   for (int j = 0; j < Qn; ++j)
     for (int i = 0; i < Pn; ++i)
       if (Q[i][j] > 0)
@@ -53,16 +69,19 @@ int main() {
   nonz.clear();
   cerr << "nnz(W) = ";
   cin >> nnz;
-  int W[Qn][Wn];
-  fill(W[0], W[0] +  Qn * Wn, 0);
-  for (int i = 0; i < nnz; ++i)
-    W[dist(g) % Qn][dist(g) % Wn] = 1;
+  //  int W[Qn][Wn];
+  //  fill(W[0], W[0] +  Qn * Wn, 0);
+  for (int i = 0; i < nnz; ++i) {
+    W[rand() % hQn][rand() % Wn] = 1;
+  }
+
   for (int j = 0; j < Wn; ++j)
     for (int i = 0; i < Qn; ++i)
       if (W[i][j] > 0)
 	nonz.push_back(Element(i, j, 1));
   cout << Qn << " " << Wn << endl;
   cout << nonz.size() << endl;
+
   for (const Element &e : nonz)
     cout << e.row << " " << e.col << " " << e.val << endl;
 
