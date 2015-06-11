beac8d2b0fa0eae0ca0d26296791800b2b94ad05
CJC <jiecchen@indiana.edu>
2015-06-11 02:35:29 -0400

    updated

--- test_dblp.cpp
+++ test_dblp.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
     std::exit(1);
   }
   CTimer timer;
-  timer.start();
+  timer.start("Read data");
   CMatrix_COO coo;
   std::ifstream fin(argv[1]);
   int M = 0;
@@ -37,27 +37,38 @@ int main(int argc, char **argv) {
   ++M;
   coo.set_mn(M, N);
   fin.close();
-  timer.stop("Read data ");
-  timer.start();
+  timer.stop();
+  timer.start("Create CSC Matrix ");
   CMatrix_CSC P(coo, false);
   CMatrix_COO cooT(coo.T());
   CMatrix_CSC Q(cooT, false);
-  timer.stop("Create CSC Matrix ");
+  timer.stop();
   std::cout << "Matrix: " << P.m << "x" << P.n << " nnz = " << P.nnz << std::endl;
   
-  double theta= 400;
-
-  timer.start();
-  CMatrix_COO&& res = toCoo(P * Q);
-  res.print(theta);
-  timer.stop("Naive P * P ");
+  double theta= 300000;
+  double rho = 0.1;
+  int w = 20;
 
 
+  // std::cerr << std::endl;
+  // timer.start("Naive P * P ");
+  // CMatrix_COO&& res = toCoo(P * Q);
+  // res.print(theta);
+  // timer.stop();
 
-  timer.start();
-  CMatrix_COO&& new_res = toCoo(FastThreshMult_Simple(P, Q, 10, theta, 0.2));
-  timer.stop("Use Our Algorithm ");
+
+  
+  // std::cerr << std::endl;
+  // timer.start("Use FastThreshMult_Simple");
+  // CMatrix_COO&& new_res = toCoo(FastThreshMult_Simple(P, Q, w, theta, rho));
+  // timer.stop();
+  
+  std::cerr << std::endl;
+  timer.start("Use FastThreshMult");
+  CMatrix_COO &&new_res = toCoo(FastThreshMult(P, Q, w, theta, rho));
+  timer.stop();
   new_res.print();
+ 
   return 0;
 }
 
